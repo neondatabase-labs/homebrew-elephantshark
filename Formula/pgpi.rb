26366f32e3d83769928ecaaf0987ff84ab6b27e3
George MacKerron <george@mackerron.co.uk>
2025-08-11 14:11:31 +0100

    Added pgpi formula

---
+++ Formula/pgpi.rb
@@ -0,0 +1,24 @@
+class Pgpi < Formula
+  desc "Postgres Private Investigator"
+  homepage "https://github.com/neondatabase-labs/pgpi"
+  url "https://github.com/neondatabase-labs/pgpi/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "93f1924284c1f8d5834690fd53b1e315e0366bf96bab503f17a833765acea8aa"
+  license "Apache-2.0"
+
+  depends_on "ruby"
+
+  def install
+    bin.install "pgpi"
+  end
+
+  test do
+    port = 54545
+    pid = spawn bin/"pgpi", "--client-listen-port", port.to_s
+    sleep 1 # takes a moment to be ready to listen
+    conn = TCPSocket.new "127.0.0.1", port
+    conn.write "\x00\x00\x00\x08\x04\xd2\x16\x2f" # send SSLRequest
+    response = conn.read 1 # read response
+    assert_equal "S", response # expect "S": SSL is supported
+    Process.kill "SIGTERM", pid
+  end
+end
